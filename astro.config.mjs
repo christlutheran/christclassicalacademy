5b7c314b4fbf7513b114d79eb8cb22412ef69045
Bryce Johnston <bjohnstondev@gmail.com>
2025-03-15 11:26:15 -0500

    timeline, support us and contact page improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://wwww.christclassicalmanhattan.org',
+  site: 'https://www.christclassicalmanhattan.org',
   output: 'static',
   server: {
     host: true
