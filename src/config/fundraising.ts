7f254306f51247171771b38deb261f53dc93c7a2
Bryce Johnston <bjohnstondev@gmail.com>
2025-07-08 19:18:17 -0500

    update about and fundraising pg

--- src/config/fundraising.ts
+++ src/config/fundraising.ts
@@ -7,7 +7,7 @@ export interface FundraisingConfig {
 
 export const fundraisingConfig: FundraisingConfig = {
   goal: 200000, // $200,000 goal
-  raised: 13936.30, // Amount raised so far
+  raised: 15000, // Amount raised so far
   startDate: '2025-03-15',
   endDate: '2026-05-15'
 };
